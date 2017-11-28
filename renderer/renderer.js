02c724496792b8e425ac6904c490a0c8c6bd3c5c
uefigod <uefigod@gmail.com>
2017-11-28 09:56:15 +0530

    can browse through browser now (spaces dont work)

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,15 +1,38 @@
 const fs = require('fs');
-/*
- readFolder function to obtain the path
+/**
+ * We create an object from electron module. The shell object allows us to open the selected file
  */
+const shell = require('electron');
+
 function readFolder(path) {
     fs.readdir(path, (err, files) => {
         'use strict';
         if (err) throw  err;
-        //the files parameter is an array of the files and folders in the path we passed. So we loop through the array, printing each file and folder
+        //Dynamically add <ol> tags to the div
+        document.getElementById('listed-files').innerHTML = `<ol id="display-files"></ol>`;
         for (let file of files) {
-            //the += after innerHTML means we are appending to the existing content
-            document.getElementById('display-files').innerHTML += `<li>${file}</li>`;
+            fs.stat(path + file, (err, stats) => {
+                /**
+                 *When you double click on a folder or file, we need to obtain the path and name so that we can use it to take action. The easiest way to obtain the path and name for each file and folder, is to store that information in the element itself, as an ID. this is possible since we cannot have two files with the same name in a folder. theID variable below is created by concatenating the path with file name and a / at the end. As indicated earlier, we must have the / at the end of the path.
+                 *
+                 */
+                let theID = `${path}${file}/`;
+                if (err) throw err;
+                if (stats.isDirectory()) {
+                    /**
+                     * Add an ondblclick event to each item. With folders, call this same function (recursion) to read the contents of the folder. If its a file, call the openFile function to open the file with the default app.
+                     *
+                     */
+                    document.getElementById('display-files').innerHTML += `<li id=${theID} ondblclick="readFolder(this.id)"><i class="fa fa-folder-open"></i> ${file}</li>`;
+                }
+                else {
+                    document.getElementById('display-files').innerHTML += `<li id=${theID} ondblclick="openFile(this.id)"><i class="fa fa-file"></i> ${file}</li>`;
+                }
+            });
         }
     });
+}
+//open the file with the default application
+function openFile(path) {
+    shell.openItem(path);
 }
